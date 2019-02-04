b03af7d3b283b1fe15aba47091376207e74adbb7
Corey Alexander <coreyja@gmail.com>
2019-02-03 19:32:37 -0500

    Make size dir

--- script/test-binary-search.sh
+++ script/test-binary-search.sh
@@ -8,6 +8,7 @@ function run_test_for_git_object {
   git checkout "$2"
   cargo build --release
   for size in "${SIZES[@]}"; do
+    mkdir -p "$BASE_DIR/$size"
     hyperfine --warmup 3 --export-markdown "$BASE_DIR/$size/$1.md" "cat ~/tmp/$size.txt | ./target/release/devicon-lookup"
   done
 
