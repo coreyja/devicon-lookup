86a5322c069c82555512309336be487687ac677a
Corey Alexander <coreyja@gmail.com>
2019-02-03 19:31:08 -0500

    Change script to include sizes

--- script/test-binary-search.sh
+++ script/test-binary-search.sh
@@ -1,24 +1,18 @@
 BASE_DIR="./results/$1/"
 
+SIZES=("small" "large")
+
 mkdir -p "$BASE_DIR"
 
 function run_test_for_git_object {
   git checkout "$2"
   cargo build --release
-  hyperfine --warmup 3 --export-markdown "$BASE_DIR/$1.md" 'cat ~/tmp/large.txt | ./target/release/devicon-lookup'
+  for size in "${SIZES[@]}"; do
+    hyperfine --warmup 3 --export-markdown "$BASE_DIR/$size/$1.md" "cat ~/tmp/$size.txt | ./target/release/devicon-lookup"
+  done
+
 }
 
-# First Baseline Run Against Master
 run_test_for_git_object "baseline" "master"
-
-# Second Plain Binary Search
 run_test_for_git_object "plain" "b931ae1"
-# git checkout b931ae1
-# cargo build --release
-# hyperfine --warmup 3 --export-markdown "$BASE_DIR/plain.md" 'cat ~/tmp/large.txt | ./target/release/devicon-lookup'
-
-# Second Plain Binary Search
 run_test_for_git_object "cached" "binary-search"
-# git checkout binary-search
-# cargo build --release
-# hyperfine --warmup 3 --export-markdown "$BASE_DIR/cached.md" 'cat ~/tmp/large.txt | ./target/release/devicon-lookup'
