6080b8efd0b42e24032e7d24c90d609697429c91
Corey Alexander <coreyja@gmail.com>
2019-03-23 19:57:41 -0400

    Use a regex to strip the ansi color codes, and bring back lazy_static to make the regex object only once

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+#[macro_use] extern crate lazy_static;
 extern crate phf;
 
-use colored::*;
 use std::ffi::OsStr;
 use std::io::{self, BufRead};
 use std::path::Path;
+use regex::Regex;
 
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
@@ -13,11 +14,18 @@ fn get_extension_from_filename(filename: &str) -> Option<&str> {
     Path::new(filename).extension().and_then(OsStr::to_str)
 }
 
+fn strip_ansi_codes(input: &str) -> String {
+  lazy_static! {
+    static ref ANSI_COLOR_REGEX: Regex = Regex::new(r"\x1b\[[0-9;]*m").unwrap();
+  }
+  ANSI_COLOR_REGEX.replace_all(input, "").to_string()
+}
+
 fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let filename = line.unwrap();
-        let decolored_filename = filename.normal().clear();
+        let decolored_filename = strip_ansi_codes(&filename);
         let extension = get_extension_from_filename(&decolored_filename);
         let symbol = match extension {
             Some(extension) => SYMBOL_MAP.get(extension).unwrap_or(&DEFAULT_SYMBOL),
