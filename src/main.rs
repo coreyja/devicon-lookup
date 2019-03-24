b3f8db4e93d3aeb3165111ed2002db4fb9fd9aeb
Corey Alexander <coreyja@gmail.com>
2019-03-23 20:05:59 -0400

    chore: AutoFormat all Code (#18)

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,21 @@ use std::path::Path;
 
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
-static DEFAULT_SYMBOL: &str = & "";
+static DEFAULT_SYMBOL: &str = &"";
 
 fn get_extension_from_filename(filename: &str) -> Option<&str> {
-	Path::new(filename)
-		.extension()
-		.and_then(OsStr::to_str)
+    Path::new(filename).extension().and_then(OsStr::to_str)
 }
 
 fn main() {
-	let stdin = io::stdin();
-	for line in stdin.lock().lines() {
-		let filename = line.unwrap();
-		let extension = get_extension_from_filename(& filename);
-		let symbol = match extension {
-			Some(extension) => SYMBOL_MAP.get(extension).unwrap_or(& DEFAULT_SYMBOL),
-			None => DEFAULT_SYMBOL
-		};
-		println!("{} {}", symbol, filename);
-	}
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let filename = line.unwrap();
+        let extension = get_extension_from_filename(&filename);
+        let symbol = match extension {
+            Some(extension) => SYMBOL_MAP.get(extension).unwrap_or(&DEFAULT_SYMBOL),
+            None => DEFAULT_SYMBOL,
+        };
+        println!("{} {}", symbol, filename);
+    }
 }
-
