be5fd83b3ec822c176f3be4f09a8156f604c504a
Corey Alexander <coreyja@gmail.com>
2019-04-02 20:46:35 -0400

    Tidy up and break out a few new methods

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ use std::path::Path;
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const USAGE: &'static str = include_str!("USAGE.txt");
 
+lazy_static! {
+    static ref ANSI_COLOR_REGEX: Regex = Regex::new(r"\x1b\[[0-9;]*m").unwrap();
+}
+
 #[derive(Debug, Deserialize)]
 struct Args {
     flag_color: bool,
@@ -26,32 +30,33 @@ fn get_extension_from_filename(filename: &str) -> Option<&str> {
     Path::new(filename).extension().and_then(OsStr::to_str)
 }
 
+fn get_symbol_from_extension(extension: &str) -> &str {
+    SYMBOL_MAP.get(extension).unwrap_or(&DEFAULT_SYMBOL)
+}
+
 fn strip_ansi_codes(input: &str) -> String {
-    lazy_static! {
-        static ref ANSI_COLOR_REGEX: Regex = Regex::new(r"\x1b\[[0-9;]*m").unwrap();
-    }
     ANSI_COLOR_REGEX.replace_all(input, "").to_string()
 }
 
-fn maybe_strip_color(strip_color: bool, input: &str) -> String {
-    if strip_color {
-        strip_ansi_codes(input)
+fn process_line(strip_color: bool, line: &str) {
+    let filename = String::from(line);
+    let file_name_to_parse = if strip_color {
+        strip_ansi_codes(&filename)
     } else {
-        String::from(input)
-    }
+        filename.clone()
+    };
+    let extension = get_extension_from_filename(&file_name_to_parse);
+    let symbol = match extension {
+        Some(extension) => get_symbol_from_extension(extension),
+        None => DEFAULT_SYMBOL,
+    };
+    println!("{} {}", symbol, filename);
 }
 
 fn process_stdin(strip_color: bool) {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-        let filename = line.unwrap();
-        let file_name_to_parse = maybe_strip_color(strip_color, &filename);
-        let extension = get_extension_from_filename(&file_name_to_parse);
-        let symbol = match extension {
-            Some(extension) => SYMBOL_MAP.get(extension).unwrap_or(&DEFAULT_SYMBOL),
-            None => DEFAULT_SYMBOL,
-        };
-        println!("{} {}", symbol, filename);
+        process_line(strip_color, &line.unwrap());
     }
 }
 
