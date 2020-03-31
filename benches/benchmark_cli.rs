a62bc8c2be743ea58c4559f25071b9dcee3e7aad
Corey Alexander <coreyja@gmail.com>
2020-03-30 20:09:37 -0400

    Make benches with larger test file (#363)

--- benches/benchmark_cli.rs
+++ benches/benchmark_cli.rs
@@ -1,16 +1,32 @@
 #[macro_use]
 extern crate criterion;
 
+use assert_cmd::prelude::*;
 use assert_cmd::Command;
 use criterion::Criterion;
 
-fn run_cli() {
-    let mut cmd = Command::cargo_bin("devicon-lookup").unwrap();
-    cmd.pipe_stdin("tests/fixtures/all-types.txt").unwrap();
+fn run_cli_plain() {
+    Command::cargo_bin("devicon-lookup")
+        .unwrap()
+        .pipe_stdin("tests/fixtures/all-types-large.txt")
+        .unwrap();
+}
+
+fn run_cli_color() {
+    Command::cargo_bin("devicon-lookup")
+        .unwrap()
+        .arg("--color")
+        .pipe_stdin("tests/fixtures/all-types-large.txt")
+        .unwrap();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("benchmark_each_file_type_cli", |b| b.iter(|| run_cli()));
+    c.bench_function("benchmark_each_file_type_plain_cli", |b| {
+        b.iter(run_cli_plain)
+    });
+    c.bench_function("benchmark_each_file_type_color_cli", |b| {
+        b.iter(run_cli_color)
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
