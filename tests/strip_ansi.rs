8536f9777c6dc6dc932c12d35d4e907ca3767fb5
Corey Alexander <coreyja@gmail.com>
2024-01-27 17:25:59 -0500

    Format vs to_string

--- tests/strip_ansi.rs
+++ tests/strip_ansi.rs
@@ -8,7 +8,7 @@ mod integration {
     #[test]
     fn calling_devicon_lookup_without_strip_color_single_file() {
         let mut cmd = Command::cargo_bin("devicon-lookup").unwrap();
-        cmd.write_stdin("test.rs".blue().to_string())
+        cmd.write_stdin(format!("{}", "test.rs".blue()))
             .assert()
             .stdout(format!(" {}\n", "test.rs".blue()));
     }
